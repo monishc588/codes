453301c1354fcb5c84cedb72c48acb6005cad1e2
monishc588 <monishdurga077@gmail.com>
2025-07-14 21:18:32 +0530

    codes

--- assignments/assignment-1.py
+++ assignments/assignment-1.py
@@ -1,1 +1,35 @@
-# 1 -
+# 1 - ask the user for their full name and print only the first name using slicing and indexing
+
+# name = input("enter the full name :")
+# first_name = name[:name.find(" ")]
+# print("your first name is :", first_name)
+
+#2 - Take the user's input for a city name and print it in uppercase, lowercase, and title case
+
+# city = input("enter the city name: ")
+# print(city.upper())
+# print(city.lower())
+# print(city.title())
+
+#3 - Print the reversed version of the string 'Technology' using slicing.
+
+# a = "technology"
+# print(a[::-1])
+
+#4 -  Combine the strings 'Data' and 'Science' with a hyphen and display the result.
+
+# a = "data"
+# b = "science"
+# c = ( a + "-" + b)
+# print(c)
+
+#5 -  Ask the user for a product price and display it formatted as currency with two decimal places.
+
+# price = float(input("enter the product price :"))
+# print(f"${price:.2f}")
+
+#6 - Store your name and age in variables, then print a sentence using f-string formatting.
+
+name = "monish"
+age = 20
+print(f"hello {name} are you {age} years old")
